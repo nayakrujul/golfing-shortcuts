cd967b3088faaa5093d832ecb9cb2a700de29435
Rujul Nayak <55329600+nayakrujul@users.noreply.github.com>
2022-09-25 12:25:30 +0100

    Create __init__.py

---
+++ s/__init__.py
@@ -0,0 +1,111 @@
+# Built-in functions
+a = any
+b = bin
+c = chr
+d = dir
+e = eval
+f = filter
+g = globals
+h = hash
+i = input
+j = all
+k = max
+l = len
+m = map
+n = enumerate
+o = ord
+p = print
+q = min
+r = range
+s = sum
+t = type
+u = locals
+v = vars
+x = exec
+y = isinstance
+z = zip
+
+# String methods
+sc = str.count
+se = str.encode
+sf = str.format
+si = str.index
+sj = str.join
+sl = str.lower
+ss = str.split
+su = str.upper
+sz = str.zfill
+
+# List methods
+la = list.append
+lc = list.count
+le = list.extend
+li = list.index
+ln = list.insert
+lp = list.pop
+lr = list.remove
+ls = list.sort
+
+# Colours
+cr = '\033[1;31m' # Red
+cg = '\033[1;32m' # Green
+cy = '\033[1;33m' # Yellow
+cb = '\033[1;34m' # Blue
+cm = '\033[1;35m' # Magenta
+cc = '\033[1;36m' # Cyan
+cw = '\033[0m'    # White
+
+# Math library
+import math
+Mac = math.acos
+Mas = math.asin
+Mat = math.atan
+MC = math.ceil
+Mc = math.cos
+Md = math.degrees
+Me = math.e
+MF = math.floor
+Mf = math.factorial
+Mg = math.gcd
+Ml = math.log
+Mp = math.pi
+Mr = math.radians
+MS = math.sqrt
+Ms = math.sin
+Mt = math.tan
+
+# Random library
+import random
+Rc = random.choice
+Rd = random.seed
+Rg = random.randrange
+Ri = random.randint
+Rp = random.sample
+Rr = random.random
+Rs = random.sample
+Ru = random.uniform
+Rx = random.choices
+
+# String library
+import string
+Sb = string.printable
+Sd = string.digits
+Sh = string.hexdigits
+Sl = string.ascii_lowercase
+So = string.octdigits
+Sp = string.punctuation
+St = string.ascii_letters
+Su = string.ascii_uppercase
+Sw = string.whitespace
+
+# Itertools library
+import itertools
+Ia = itertools.accumulate
+Ib = itertools.combinations
+Ic = itertools.count
+Ih = itertools.chain
+Im = itertools.permutations
+Ip = itertools.product
+Ir = itertools.repeat
+Iw = itertools.combinations_with_replacement
+Iy = itertools.cycle
